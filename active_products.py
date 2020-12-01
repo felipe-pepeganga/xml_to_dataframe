d954bee062cdad4ead25021de2f2276eea6e4151
David Felipe Valbuena <david.valbuena@pepeganga.net>
2020-12-01 08:12:55 -0500

    0 a plu mod

--- active_products.py
+++ active_products.py
@@ -5,6 +5,9 @@ import json
 import xml.etree.ElementTree as ET
 import pandas as pd
 import os
+from csv import writer
+from csv import reader
+import csv
 
 def exportar_csv():
     #PoolManager() instancia para realizar solicitudes
@@ -12,29 +15,50 @@ def exportar_csv():
     #Obtener xml
     r = http.request('GET', 'https://www.pepeganga.com/XMLData/test-impresee.xml')
     #Respuesta de la llamada
-    print(r.status)
     #Diccionario JSON identado
     dict = json.dumps(xmltodict.parse(r.data), indent=4, sort_keys=True)
     diction_json = json.loads(dict)
     #items = diction_json.items()
     dict_keys = list(diction_json["Feed"]["Item"][0].keys())
-    """print(dict_keys)
-    #print(items[0])
-    for item in diction_json["Feed"]["Item"]:
-        #print(key, ":", diction_json[key])
-        #print(item)
-        break
-    #print(diction_json)
-    
-    print("////")
-    
-    for i in dict_keys:
-        print(i)"""
-
     df = pd.DataFrame(diction_json["Feed"]["Item"], columns=dict_keys)
     path = str(os.path.dirname(__file__))
     df.to_csv(path+'/dataframe.csv', index = False, header=True)
-    print("Termino.")
+
+def plu():
+    csv = pd.read_csv("dataframe.csv")
+    df = pd.DataFrame(csv)
+    df['Plu'] = df['Plu'].astype(str)
+    TK=df['Plu'].astype(str)
+    df=df.assign(Plu_mod=plu_mod(TK.values))
+    path = str(os.path.dirname(__file__))
+    df.to_csv(path+'/dataframe_mod.csv', index = False, header=True)
+    
+def plu_mod(plus):
+    plus_mod = []
+    for plu in plus:
+        numS = str(plu)     
+        if len(numS) < 6:
+            if len(numS) == 1:
+                plu = "00000" + str(plu)
+                plus_mod.append(plu)
+            if len(numS) == 2:
+                plu = "0000" + str(plu)
+                plus_mod.append(plu)
+            if len(numS) == 3:
+                plu = "000" + str(plu)
+                plus_mod.append(plu)
+            if len(numS) == 4:
+                plu = "00" + str(plu)
+                plus_mod.append(plu)
+            if len(numS) == 5:
+                plu = "0" + str(plu)
+                plus_mod.append(plu)
+        else:
+            plus_mod.append(plu)
+
+    for plu in plus_mod:
+        print(plu)
+    return plus_mod
 
 
 
--- gui.py
+++ gui.py
@@ -1,21 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
-from active_products import *
+import active_products as ap
 import os
 
 def call_export():
-    exportar_csv()
+    ap.exportar_csv()
     messagebox.showinfo("Informacion", "archivo exportado.")
+def call_plu():
+    ap.plu()
+    messagebox.showinfo("Informacion", "archivo modificado.")
+    
 raiz = Tk()
 raiz.title("Conversion de xml a dataframe")
 raiz.resizable(1,1)
 raiz.geometry("225x225")
-path = str(os.path.dirname(__file__))
+
+"""path = str(os.path.dirname(__file__))
+print(path)
 img = PhotoImage(file=path+'/descarga.png')
 background_label = Label(raiz, image=img)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
-raiz.tk.call('wm', 'iconphoto', raiz._w, img)
+raiz.tk.call('wm', 'iconphoto', raiz._w, img)"""
 exportar = Button(raiz, text='Exportar', command=call_export).grid(row=4, column=1)
+plu = Button(raiz, text='0 PLUS', command=call_plu).grid(row=4, column=2)
 """frame = Frame()
 frame.pack()
 frame.config(bg="red")
